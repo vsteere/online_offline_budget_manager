0b83fb8088ea751b471ec3c5c70e5bfde3c2b652
vsteere <vssteere@gmail.com>
2020-11-07 14:50:47 -0600

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget2',
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget2',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
